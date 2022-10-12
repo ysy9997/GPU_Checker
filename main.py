45cfc3235dccd66f351996960d62da7aacc1d2c6
ysy9997 <ysy9997@gmail.com>
2022-10-12 10:49:41 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from pexpect import pxssh
+import json
+
+with open('server.json', 'r') as f:
+    cf = json.load(f)
+
+for key in cf.keys():
+    s = pxssh.pxssh()
+    s.login(cf[key]['host'], cf[key]['user'], bytes(cf[key]['password'], 'utf-8'), port=cf[key]['port'])
+    s.sendline('nvidia-smi')
+    s.prompt(1)
+    line = s.before.decode('utf-8')
+    print(line.split('                                                                               ')[0])
+    s.logout()
